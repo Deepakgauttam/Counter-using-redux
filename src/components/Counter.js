4be4902565ad631a130add5db8a97177ca838ee6
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2024-08-12 23:49:26 +0530

    decrement woking

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addCounter, resetCounter } from "../redux/actions/index";
+import { addCounter, subCounter, resetCounter } from "../redux/actions/index";
 
 const Counter = () => {
-  const { amount } = useSelector(state => state.counter);
+  const { amount } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const handleAdd = () => {
     dispatch(addCounter());
+  };
+
+  const handleSub = () => {
+    if (amount > 0) {
+      dispatch(subCounter());
+    } else {
+      return;
+    }
   };
 
   const handleReset = () => {
@@ -18,6 +26,7 @@ const Counter = () => {
     <div>
       <h4>{amount}</h4>
       <button onClick={handleAdd}> ADD </button>
+      <button onClick={handleSub}> SUB </button>
       <button onClick={handleReset}> RESET </button>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { createStore } from "redux";
 import RootReducer from "./reducers/index";
 
-const store = createStore(RootReducer); 
+const store = createStore(
+  RootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
+); 
 export default store;
